59caeefcc49d54ff7aaff4f954ee462c7f886665
ocrzia <hello@olivier-ceressia.be>
2025-05-20 15:56:45 +0200

    feat: initialize project structure with HTML, CSS, and JavaScript files
    - Added index.html with basic structure and linked CSS and JS files.
    - Created main.js to handle player actions and score calculations.
    - Set up SCSS structure with abstracts, base, components, layout, and pages.
    - Implemented a reset stylesheet for consistent styling across browsers.
    - Compiled styles into styles.min.css and generated source map.

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// Un beau tableau d'objets
+const actions = [
+  { joueur: "Léa", points: 10 },
+  { joueur: "Tom", points: 5 },
+  { joueur: "Léa", points: 7 },
+  { joueur: "Nina", points: 12 },
+  { joueur: "Tom", points: 3 },
+];
+
+// Un objet vide
+const scores = {};
+
+// On boucle sur le tableau
+actions.forEach((action) => {
+  const nom = action.joueur;    // On isole le nom d'un joueur dans une variable
+
+  if (!scores[nom]) {           // On vérifie si le nom est déjà présent sous forme de clé dans l'objet scores
+    scores[nom] = 0;            // On attribue une valeur de 0 pour initiliser la valeur du score du joueur à 0 s'il n'existait pas
+  }
+  scores[nom] += action.points; // On ajoute les points du joueur à ses points actuels
+})
+
+console.log(scores);
